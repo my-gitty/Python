ac9b440dc147e26995333bda2256ff148aecdb42
WeigangZhu <Zhu_Dean@outlook.com>
2018-01-02 09:40:25 +0800

    class method

---
+++ introduction_python/class_method/class_method_A.py
@@ -0,0 +1,17 @@
+class A():
+	count = 0
+	def __init__(self):
+		A.count += 1
+	
+	def exclaim(self):
+		print("I'm an A!")
+
+	@classmethod
+	def kids(cls):
+		print("A has", cls.count, "little objects.")
+
+if __name__ == '__main__':
+	easy_a = A()
+	breeze_a = A()
+	wheezy_a = A()
+	print( 'A.kids() = ', A.kids() )
