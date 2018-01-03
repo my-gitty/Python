61fccf72fca81f898ecded8ac88376b14e49140c
WeigangZhu <Zhu_Dean@outlook.com>
2018-01-03 10:24:49 +0800

    add the explain of the 'a' and 'herf' in the HTML

--- introduction_python/web/beautifulsoup.py
+++ introduction_python/web/beautifulsoup.py
@@ -4,6 +4,8 @@ def get_links(url):
 	result = requests.get(url)
 	page = result.text
 	doc = soup(page)
+	
+	# the 'a' express a link of the HTML and the 'herf' express a address of the link.
 	links = [ element.get('href') for element in doc.find_all('a')]
 	return links
 
