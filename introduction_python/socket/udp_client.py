a2ace69b0446440b552af33651296d5828b90b9e
WeigangZhu <Zhu_Dean@outlook.com>
2018-04-29 13:57:56 +0800

    add the sleep function to control the client to send message in a fixed time

--- introduction_python/socket/udp_client.py
+++ introduction_python/socket/udp_client.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 import socket
+import time
 
 server_address = ('localhost', 6789)
 max_size = 4096
 
 print("Starting the client at", datetime.now())
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(b'Hey!', server_address)
+while True:
+	client.sendto(b'Hey!', server_address)
+	time.sleep(1)
+	
 
 data, server =client.recvfrom(max_size)
 
